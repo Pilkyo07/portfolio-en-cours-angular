0c2e8b65230467605c382ff92210e811d3a717a1
lucas <lucas@localhost.com>
2025-01-08 22:09:04 +0100

    Premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:HomeComponent},
+    {path:"contact", component:ContactComponent},
+    {path:"about", component:AboutComponent},
+    {path:"skills", component:SkillsComponent},
+    {path:"portfolio", component:PortfolioComponent}
+];
